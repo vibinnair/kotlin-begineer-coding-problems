4acddfada93ed1e3769c8f2df70b89b4da07a0be
Vibin Nair <Vibin.Nair@gmail.com>
2018-09-25 17:14:00 +0530

    vnair: Type of variables.

---
+++ kotlinExamples/src/main/kotlin/kotlinExamples/Exercise1_Types.kt
@@ -0,0 +1,13 @@
+package kotlinExamples
+
+fun main(args: Array<String>) {
+    val firstValue = 10
+    val secondValue = 20
+
+    var displayValue = firstValue + secondValue
+    println("Sum of $firstValue and $secondValue = $displayValue")
+
+    displayValue = firstValue * secondValue
+    println("Sum of $firstValue and $secondValue = $displayValue")
+}
+
