6a8844242abb7c716d1037a6915335edf92233dd
Vibin Nair <Vibin.Nair@gmail.com>
2018-09-25 19:23:14 +0530

    vnair: Exercise_1_1 solution added.

---
+++ kotlinExamples/src/main/kotlin/kotlinExamples/Exercise_1_1_surface_parameter.kt
@@ -0,0 +1,9 @@
+package kotlinExamples
+
+fun main(args: Array<String>) {
+    val side = 50
+    val surfaceArea = side * side
+    val perimeter = 4 * side
+
+    println("For the square with side $side, the area is $surfaceArea and perimeter is $perimeter")
+}
