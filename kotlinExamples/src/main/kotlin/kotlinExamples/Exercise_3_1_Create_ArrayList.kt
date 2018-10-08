992dbc25d9a39d38ea705e2f07bd107dd9c988f5
Vibin Nair <Vibin.Nair@gmail.com>
2018-10-08 13:43:46 +0530

    vnair: Exercise_3_1 solution added.

---
+++ kotlinExamples/src/main/kotlin/kotlinExamples/Exercise_3_1_Create_ArrayList.kt
@@ -0,0 +1,10 @@
+package kotlinExamples
+
+fun main(args: Array<String>) {
+    val colors = listOf("Red", "Green", "Blue", "Black", "White")
+
+    for (color in colors) {
+        println("$color")
+    }
+}
+
